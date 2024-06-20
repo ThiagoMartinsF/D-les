5a8f263d70b847a0c3b8dff43d84a724a5264a7d
ThiagoMartinsF <161362774+ThiagoMartinsF@users.noreply.github.com>
2024-06-19 22:28:55 -0300

    Update Conexao.java

--- src/java/conexao/Conexao.java
+++ src/java/conexao/Conexao.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 public class Conexao {
-        private static final  String URL = "jdbc:mysql://db4free.net:3306/bonappetit2?useSSL=false";
-        private static final  String USUARIO = "thiago_martins2";
-        private static final  String SENHA = "33429851thiago";
+        private static final  String URL = "jdbc:mysql://localhost:3306/bonappetit?useSSL=false";
+        private static final  String USUARIO = "root";
+        private static final  String SENHA = "root";
         private static final  String driver = "com.mysql.cj.jdbc.Driver";
        
 public static java.sql.Connection conectar() {
